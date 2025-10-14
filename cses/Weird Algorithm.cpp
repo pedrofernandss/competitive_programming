084c5bfd87001ff07f97d0566de1fd39e1c16c66
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-10-14 16:19:47 -0300

    CSES - Weird Algorithm

---
+++ cses/Weird Algorithm.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = (n * 3) + 1;
+        }
+    }
+
+    cout << "1" << endl;
+
+    return 0;
+}
