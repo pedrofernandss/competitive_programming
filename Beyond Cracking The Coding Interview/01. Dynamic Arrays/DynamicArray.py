e89c5be5a2bc1f5fc9c895b685e3e88caa4d8679
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-03 20:05:17 -0300

    fixing syntax

--- Beyond Cracking The Coding Interview/01. Dynamic Arrays/DynamicArray.py
+++ Beyond Cracking The Coding Interview/01. Dynamic Arrays/DynamicArray.py
@@ -1,39 +1,38 @@
 class DynamicArray:
-  def __init__(self):
-    self.capacity = 10
-    self._size = 0
-    self.fixed_array = [None]
+    def __init__(self):
+        self.capacity = 10
+        self._size = 0
+        self.fixed_array = [None] * self.capacity
 
-def get(self, int: idx):
-    if(idx < 0 || idx >= self._size):
-        raise IndexError('Index out of bounds')
-    return self.fixed_array[idx]
+    def get(self, idx):
+        if idx < 0 or idx >= self._size:
+            raise IndexError('Index out of bounds')
+        return self.fixed_array[idx]
 
-def set(self, int: idx, int: value):
-    if(idx < 0 || idx >= self._size):
-        raise IndexError('Index out of bounds')
-    return self.fixed_array[idx] = value
+    def set(self, idx, value):
+        if idx < 0 or idx >= self._size:
+            raise IndexError('Index out of bounds')
+        self.fixed_array[idx] = value
 
-def size():
-    return self._size
-    
-def resize(self, int: new_capacity):
-    new_fixed_size_arr = [None] * new_capacity
-    for idx in range(self._size):
-        new_fixed_size_arr[idx] = self.fixed_array[idx]
-    self.fixed_array = new_fixed_size_arr
-    self.capacity = new_capacity
+    def size(self):
+        return self._size
+        
+    def resize(self, new_capacity):
+        new_fixed_size_arr = [None] * new_capacity
+        for idx in range(self._size):
+            new_fixed_size_arr[idx] = self.fixed_array[idx]
+        self.fixed_array = new_fixed_size_arr
+        self.capacity = new_capacity
 
-def append(self, int: value):
-    if self._size == self.capacity:
-        self.resize(capacity*2)
-    self.fixed_array[self._size] = x
-    self._size += 1
+    def append(self, value):
+        if self._size == self.capacity:
+            self.resize(self.capacity*2)
+        self.fixed_array[self._size] = value
+        self._size += 1
 
-def pop_back(self):
-    if self._size == 0:
-        raise IndexError('Pop from empty array')
-    self._size -= 1
-    if (self._size/self.capacity) < 0.25 and self.capacity > 10:
-        self.resize(self.capacity//2)
-    
+    def pop_back(self):
+        if self._size == 0:
+            raise IndexError('Pop from empty array')
+        self._size -= 1
+        if (self._size/self.capacity) < 0.25 and self.capacity > 10:
+            self.resize(self.capacity//2)
