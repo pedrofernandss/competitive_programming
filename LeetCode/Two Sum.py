143677f14619fc62fa4185ed87acc9fb8727c8c1
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-04 19:59:37 -0300

    Two Sum Improved

--- LeetCode/Two Sum.py
+++ LeetCode/Two Sum.py
@@ -1,5 +1,9 @@
 def twoSum(nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-                for j in range(i+1, len(nums)):
-                    if nums[i] + nums[j] == target:
-                            return [i, j]
+        hashmap = {}
+        for idx in range(len(nums)):
+                hashmap[nums[idx]] = idx
+        for idx in range(len(nums)):
+                complement = target - nums[idx]
+                if complement in hashmap and hashmap[complement] != idx:
+                       return [idx, hashmap[complement]] 
+        return []
