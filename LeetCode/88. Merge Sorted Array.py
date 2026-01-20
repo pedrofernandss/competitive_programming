9b1399af8dbc3e69c494ac2a66eb74fecf2e836a
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-20 09:58:41 -0300

    88. Merge Sorted Array

---
+++ LeetCode/88. Merge Sorted Array.py
@@ -0,0 +1,7 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        right = 0
+        for i in range(len(nums1)):
+            if nums1[i] == 0:
+                nums1[i] = nums2[right]
+                right += 1
+        nums1.sort()   
