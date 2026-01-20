cf6f1330cf3755900ca3660d9dc174a92050dc7e
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-01-20 10:00:41 -0300

    Fixing bound 88. Merge Sorted Array

--- LeetCode/88. Merge Sorted Array.py
+++ LeetCode/88. Merge Sorted Array.py
@@ -1,6 +1,6 @@
 def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         right = 0
-        for i in range(len(nums1)):
+        for i in range(m, len(nums1)):
             if nums1[i] == 0:
                 nums1[i] = nums2[right]
                 right += 1
