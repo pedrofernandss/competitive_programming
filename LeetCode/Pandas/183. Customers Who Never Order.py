395a3903a18bc45496f5f5d21c9e3df3cde6fe29
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-07-21 14:28:35 -0300

    183. Customers Who Never Order

---
+++ LeetCode/Pandas/183. Customers Who Never Order.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+   df = pd.concat(['customers', 'orders']).drop_duplicates('name', keep=False)
+   return(df)
